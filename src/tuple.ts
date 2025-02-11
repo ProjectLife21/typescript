80dc9b6dcdef0de7adccb6c61da52e6544223d78
ProjectLife21 <projectlife21@gmail.com>
2025-02-11 11:28:25 +0100

    feature: tuple

---
+++ src/tuple.ts
@@ -0,0 +1,14 @@
+let person: [string, number] = ["john", 25];
+
+let date: readonly [number, number, number] = [12, 17, 2001];
+console.log(date);
+
+function getPerson(): [string, number] {
+  return ["john", 25];
+}
+
+let randomPerson = getPerson();
+console.log(randomPerson[0]);
+console.log(randomPerson[1]);
+
+let susan: [string, number?] = ["susan"];
