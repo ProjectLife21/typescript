6f68b19244cbd35ad92840f57a9b3353cbd2516f
ProjectLife21 <projectlife21@gmail.com>
2025-02-11 08:14:11 +0100

    feature: type annotations

---
+++ src/typeAnnotations.ts
@@ -0,0 +1,15 @@
+//string
+export let awesomeName: string = "shakeAndBake";
+awesomeName = "something";
+awesomeName = awesomeName.toUpperCase();
+console.log(awesomeName);
+
+//number
+let amount: number = 20;
+amount = 12 - 1;
+// amount = 'pants';
+
+//boolean
+let isAwesome: boolean = true;
+isAwesome = false;
+// isAwesome = 15;
