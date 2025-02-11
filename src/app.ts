73da4d38cc69829d8799e0767b0f3d83f45ccc60
ProjectLife21 <projectlife21@gmail.com>
2025-02-11 08:32:27 +0100

    feature: arrays

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,3 @@
-const books = ["1984", "Brave New World", "Pan Tadeusz"];
+import { myArray } from "./arrays";
 
-// let foundBook: string = "";
-let foundBook: string | undefined;
-
-for (let book of books) {
-  if (book === "Pan Tadeusz") {
-    foundBook = book;
-    foundBook.length;
-    break;
-  }
-}
-
-console.log("Found book is " + foundBook);
-console.log("Book length is " + foundBook?.length);
+myArray;
---
+++ src/arrays.ts
@@ -0,0 +1,5 @@
+export let myArray: (number | string)[] = [1, 2, 3, "tom"];
+
+for (let val of myArray) {
+  console.log(val);
+}
