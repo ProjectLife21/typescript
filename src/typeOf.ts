db6a30a478bb18858f5b16d9bafc7c09b900bf93
ProjectLife21 <projectlife21@gmail.com>
2025-02-12 09:08:40 +0100

    feature: typeOf

---
+++ src/typeOf.ts
@@ -0,0 +1,22 @@
+type ValueType = string | number | boolean;
+
+let value: ValueType;
+
+const random = Math.random();
+value = random < 0.33 ? "Hello" : random < 0.66 ? 123.456 : true;
+
+checkValue(value);
+
+function checkValue(value: ValueType): void {
+  if (typeof value === "string") {
+    console.log(value.toLowerCase());
+    return;
+  }
+
+  if (typeof value === "number") {
+    console.log(value.toFixed(2));
+    return;
+  }
+
+  console.log("boolean: " + value);
+}
